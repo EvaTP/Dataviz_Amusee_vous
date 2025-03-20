c32fb22341aaa1535e6a3b286669455dfcbb2491
VitryAhmed <aayari97@gmail.com>
2025-03-20 12:36:22 +0100

    Artists.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,9 @@
 
 const findMuseum = document.querySelector("#findMuseum");
 const museumMap = document.querySelector("#museumMap");
-const artists = document.querySelector("#artists");
+
 const listMuseum = document.querySelector("#museumsList");
-const listArtists = document.querySelector("#artists");
+
 
 
 // LES FONCTIONS : ---------------------------------------------------------------
@@ -43,29 +43,3 @@ function showMuseum(musee) {
 }
 
   // Fonction qui affiche tous les artistes (API joconde) :
-async function getArtists() {
-  const response = await fetch("https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/musees-de-france-base-museofile/records?select=artiste&where=artiste%20!%3D%20%22null%22&limit=20");
-  const newData = await response.json();
-  showArtists(newData);
-}
-
-  // Appel de cette fonction : 
-getArtists();
-
-  // Fonction qui intègre dans le HTML nom de l'artiste :
-function showArtists() {
-  listArtists.innerHTML = "";
-
-  for (let i = 0; i < artists.results.length; i++) {
-    const divArtist = document.createElement("div");
-    divArtist.classList.add("artistDiv");
-    divArtist.innerText = artists.results[i].artiste;
-    listArtists.appendChild(divArtist);
-  }
-}
-// HTML
-// const mainContainer = document.querySelector("#main-container");
-// const nomMusee = document.querySelector("#nom-musee");
-// const lieu = document.querySelector("#lieu");
-// console.log('🐯 main-container :', data);
-//https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/musees-de-france-base-museofile/records?select=artiste&where=artiste%20!%3D%20%22null%22&limit=20
