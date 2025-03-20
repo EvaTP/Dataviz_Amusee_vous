419b69fd5b2374c9685e667dc57bab44c524368c
EvaTP <evathpr@icloud.com>
2025-03-20 17:51:29 +0100

    harmonisation headers

--- scripts/scriptSearch.js
+++ scripts/scriptSearch.js
@@ -27,36 +27,92 @@ function showByRegion(region){
 	listMuseum.innerHTML = "";
 
   for (let i = 0; i < region.length; i++) {
+    
     const divRegion = document.createElement("div");
     const museName = document.createElement("h3");
-    const adresse = document.createElement("p");
-    const telephone = document.createElement("p");
-    const siteWeb = document.createElement("p");
-    const resume = document.createElement("p");
-    const themes = document.createElement("p");
+    const moreInfoButton = document.createElement("button");
+
+    // const divRegion = document.createElement("div");
+    // const museName = document.createElement("h3");
+    // const adresse = document.createElement("p");
+    // const telephone = document.createElement("p");
+    // const siteWeb = document.createElement("p");
+    // const resume = document.createElement("p");
+    // const themes = document.createElement("p");
     
     divRegion.classList.add("museumDiv");
     museName.classList.add("museumName");
-    adresse.classList.add("museumAddress");
-    telephone.classList.add("museumCity");
-    siteWeb.classList.add("museumCity");
-    resume.classList.add("museumResume");
-    themes.classList.add("themesMusees");
+    moreInfoButton.classList.add("moreInfoButton");
+
+    // adresse.classList.add("museumAddress");
+    // telephone.classList.add("museumCity");
+    // siteWeb.classList.add("museumCity");
+    // resume.classList.add("museumResume");
+    // themes.classList.add("themesMusees");
 
     museName.innerText = region[i].nom_officiel;
-    adresse.innerText = `${region[i].adresse},${region[i].code_postal}, ${region[i].ville}`;
-    telephone.innerText = `Téléphone :  ${region[i].telephone}`;
-    siteWeb.innerText = `Site web :  ${region[i].url}`;
-    resume.innerText = `Histoire : ${region[i].histoire}`;
-    themes.innerText = `Thématiques associées : ${region[i].themes}`;
-    
+    moreInfoButton.innerText = "En savoir +";
+
+
+    // museName.innerText = region[i].nom_officiel;
+    // adresse.innerText = `${region[i].adresse},${region[i].code_postal}, ${region[i].ville}`;
+    // telephone.innerText = `Téléphone :  ${region[i].telephone}`;
+    // siteWeb.innerText = `Site web :  ${region[i].url}`;
+    // resume.innerText = `Histoire : ${region[i].histoire}`;
+    // themes.innerText = `Thématiques associées : ${region[i].themes}`;
+
+  // Ajouter un gestionnaire d'événements pour le bouton "En savoir plus"
+  moreInfoButton.addEventListener("click", () => {
+    openModal(region[i]);
+  });
+
     listMuseum.appendChild(divRegion);
     divRegion.appendChild(museName);
-    divRegion.appendChild(adresse);
-    divRegion.appendChild(telephone);
-    divRegion.appendChild(siteWeb);
-    divRegion.appendChild(resume);
-    divRegion.appendChild(themes);
+    divRegion.appendChild(moreInfoButton);
+ 
+
+    // listMuseum.appendChild(divRegion);
+    // divRegion.appendChild(museName);
+    // divRegion.appendChild(adresse);
+    // divRegion.appendChild(telephone);
+    // divRegion.appendChild(siteWeb);
+    // divRegion.appendChild(resume);
+    // divRegion.appendChild(themes);
   }
+}
+
+
+
+// Fonction pour ouvrir la modale
+function openModal(museum) {
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+
+  const modalContent = document.createElement("div");
+  modalContent.classList.add("modal-content");
+
+  const closeButton = document.createElement("span");
+  closeButton.classList.add("close-button");
+  closeButton.innerHTML = "&times;";
+
+  const museumDetails = document.createElement("div");
+  museumDetails.innerHTML = `
+
+    <h3 class="museumName">${museum.nom_officiel}</h3>
+    <p class="museumAddress">Adresse: ${museum.adresse}, ${museum.code_postal}, ${museum.ville}</p>
+    <p class="museumCity">Téléphone: ${museum.telephone || "Non disponible"}</p>
+    <p class="museumCity">Site web: ${museum.url || "Non disponible"}</p>
+    <p class="museumResume">Histoire: ${museum.histoire || "Non disponible"}</p>
+    <p class="themesMusees">Thèmes: ${museum.themes || "Non disponible"}</p>
+  `;
+
+  closeButton.addEventListener("click", () => {
+    modal.remove();
+  });
+
+  modalContent.appendChild(closeButton);
+  modalContent.appendChild(museumDetails);
+  modal.appendChild(modalContent);
+  document.body.appendChild(modal);
 }
 
