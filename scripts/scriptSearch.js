37f4a06ab9f485e83f61be966c504d79edf6ec86
EvaTP <evathpr@icloud.com>
2025-03-24 17:28:21 +0100

    rajout icones et affichage icone themes

--- scripts/scriptSearch.js
+++ scripts/scriptSearch.js
@@ -3,13 +3,17 @@
 const selectRegion = document.querySelector("#recherche-region");
 const selectTheme = document.querySelector("#recherche-thematique");
 const listMuseum = document.querySelector("#museumsList");
-const apiUrl = "https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/musees-de-france-base-museofile/records";
+const resultsCountDiv = document.querySelector('#resultsCount');
+const apiUrl =
+  "https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/musees-de-france-base-museofile/records";
 
 // LES FONCTIONS : ---------------------------------------------------------------
 
 // Fonction qui affiche TOUS LES MUSEES (API muséophile) :
 async function getMuseums() {
-  const response = await fetch("https://www.data.gouv.fr/fr/datasets/r/a5c5d76e-979a-4073-ba0d-0844bb3c1398");
+  const response = await fetch(
+    "https://www.data.gouv.fr/fr/datasets/r/a5c5d76e-979a-4073-ba0d-0844bb3c1398"
+  );
   const data = await response.json();
   showMuseum(data);
 }
@@ -20,33 +24,121 @@ getMuseums();
 function showMuseum(museum) {
   listMuseum.innerHTML = "";
   for (let i = 0; i < museum.length; i++) {
-      const newDiv = document.createElement("div");
-      const museumName = document.createElement("h3");
-      const museumCity = document.createElement("h3");
-      const newImage = document.createElement("img");
-      const moreInfoButton = document.createElement("button");
+    const newDiv = document.createElement("div");
+    const museumName = document.createElement("h3");
+    const museumCity = document.createElement("h3");
+    const newImage = document.createElement("img");
+    const moreInfoButton = document.createElement("button");
 
-      newDiv.classList.add("museumDiv");
-      museumName.classList.add("museumName");
-      museumCity.classList.add("museumCity");
-      newImage.classList.add("museumImage");
-      moreInfoButton.classList.add("moreInfoButton");
+    newDiv.classList.add("museumDiv");
+    museumName.classList.add("museumName");
+    museumCity.classList.add("museumCity");
+    newImage.classList.add("museumImage");
+    moreInfoButton.classList.add("moreInfoButton");
 
-      museumName.innerText = museum[i].nom_officiel;
-      museumCity.innerText = `${museum[i].ville} (${museum[i].code_postal})`;
-      newImage.src = "./images/ticket_musee.png";
-      moreInfoButton.innerText = "En savoir +";
+    museumName.innerText = museum[i].nom_officiel;
+    museumCity.innerText = `${museum[i].ville} (${museum[i].code_postal})`;
+    newImage.src = showIcon(selectTheme)
+    moreInfoButton.innerText = "En savoir +";
 
-      listMuseum.appendChild(newDiv);
-      newDiv.appendChild(museumName);
-      newDiv.appendChild(museumCity);
-      newDiv.appendChild(newImage);
-      newDiv.appendChild(moreInfoButton);
+    listMuseum.appendChild(newDiv);
+    newDiv.appendChild(museumName);
+    newDiv.appendChild(museumCity);
+    newDiv.appendChild(newImage);
+    newDiv.appendChild(moreInfoButton);
 
-      // Ajouter un gestionnaire d'événements pour le bouton "En savoir plus"
-      moreInfoButton.addEventListener("click", () => {
-          openModal(museum[i]);
-      });
+    // Ajouter un gestionnaire d'événements pour le bouton "En savoir plus"
+    moreInfoButton.addEventListener("click", () => {
+      openModal(museum[i]);
+    });
+  }
+}
+// afficher les icones thematiques
+function showIcon(theme) {
+  if (theme) {
+    console.log(theme.value);
+    switch (theme.value) {
+      case "where=domaine_thematique%3D%22archéologie%20du%20bâti%22OR%22archéologie%22":
+        return "./icones/archeologie.png";
+        break;
+      case "where=domaine_thematique%3D%22Arts%20décoratifs%22":
+        console.log("Arts Décoratifs");
+        return "./icones/artsdecoratifs.png";
+        break;
+      case "where=domaine_thematique%3D%22Egyptien%22":
+        return "./icones/egypte.png";
+        break;
+      case "where=domaine_thematique%3D%22Gallo-romain%22":
+        return "./icones/galloromain.png";
+        break;
+      case "where=domaine_thematique%3D%22Grec%22":
+        return "./icones/grece.png";
+        break;
+      case "where=domaine_thematique%3D%22Arts%20de%20l%27Islam%22&refine=domaine_thematique%3A%22Arts%20de%20l%27Islam%22":
+        return "./icones/islam.png";
+        break;
+      case "where=domaine_thematique%3D%22Art%20moderne%20et%20contemporain%22":
+        return "./icones/artmoderne.png";
+        break;
+      case "where=domaine_thematique%3D%22Beaux-arts%22":
+        return "./icones/beauxartspeinture.png";
+        break;
+      case "where=domaine_thematique%3D%22Design%22%20OR%20domaine_thematique%3D%22Mode%22":
+        return "./icones/modedesign.png";
+        break;
+      case "where=domaine_thematique%3D%22Ethnologie%22":
+        return "./icones/ethnologie.png";
+        break;
+      case "where=domaine_thematique%3D%22Protohistoire%22%20OR%20domaine_thematique%3D%22Histoire%22":
+        return "./icones/histoire.png";
+        break;
+      case "where=domaine_thematique%3D%22Littérature%22":
+        return "./icones/litterature.png";
+        break;
+      case "where=domaine_thematique%3D%22Militaria%22":
+        return "./icones/militaria.png";
+        break;
+      case "where=domaine_thematique%3D%22Musique%20-%20chant%20-%20danse%22":
+        return "./icones/musiquechantdanse.png";
+        break;
+      case "where=domaine_thematique%3D%22Mémoire%20de%20l%27esclavage%22OR%22Esclavage%2C%20société%20de%20plantation%2C%20histoire%20de%20La%20Réunion%2C%20colonisation%2C%20industrie%20sucrière%22":
+        return "./icones/esclavage.png";
+        break;
+      case "where=domaine_thematique%3D%22Numismatique%22":
+        return "./icones/numismatique.png";
+        break;
+      case "where=domaine_thematique%3D%22Peinture%22":
+        return "./icones/beauxartspeinture.png";
+        break;
+      case "where=domaine_thematique%3D%22Photographie%22":
+        return "./icones/photo.png";
+        break;
+      case "where=domaine_thematique%3D%22Sciences%20de%20la%20nature%22":
+        return "./icones/sciences.png";
+        break;
+      case "where=domaine_thematique%3D%22Sciences%20fondamentales%22":
+        return "./icones/sciences.png";
+        break;
+      case "where=domaine_thematique%3D%22Technique%20et%20industrie%22":
+        return "./icones/technique-industrie.png";
+        break;
+      case "select=domaine_thematique&where=domaine_thematique%3D%22Afrique%22":
+        return "./icones/afrique.png";
+        break;
+      case "select=domaine_thematique&where=domaine_thematique%3D%22Amérique%22":
+        return "./icones/amerique.png";
+        break;
+      case "select=domaine_thematique&where=domaine_thematique%3D%22Asie%22":
+        return "./icones/asie.png";
+        break;
+      case "select=domaine_thematique&where=domaine_thematique%3D%22Océanie%22":
+        return "./icones/oceanie.png";
+        break;
+
+      default:
+        return "./icones/ticketMusee.png";
+        console.log("Désolé, nous n'avons plus de " + theme + ".");
+    }
   }
 }
 
@@ -73,7 +165,7 @@ function openModal(museum) {
   `;
 
   closeButton.addEventListener("click", () => {
-      modal.remove();
+    modal.remove();
   });
 
   modalContent.appendChild(closeButton);
@@ -81,88 +173,84 @@ function openModal(museum) {
   modal.appendChild(modalContent);
   document.body.appendChild(modal);
 
-  document.addEventListener('click', (event) => {
-      if (event.target == modal) {
-          modal.remove()
-      };
-  })
+  document.addEventListener("click", (event) => {
+    if (event.target == modal) {
+      modal.remove();
+    }
+  });
 }
 // ____________________________________________________________
 
 // Fonction afficher les résultats des menus déroulants REGION et THEMATIQUE
 // on ajoute l'url de base de l'API (apiUrl) à la châine de requête ("?")
 
-async function showSearchResults(){
-
-  listMuseum.innerHTML = "";   // rajout de ceci pour effacer la précédente recherche
+async function showSearchResults() {
+  listMuseum.innerHTML = ""; // rajout de ceci pour effacer la précédente recherche
 
   let regionQuery = selectRegion.value;
   let themeQuery = selectTheme.value;
   let fullUrl = apiUrl;
 
-    // construire l'url avec les filtres sélectionnés
-    if(regionQuery || themeQuery){
-      fullUrl += "?";
-      if(regionQuery){
-        fullUrl += regionQuery;
-        if(themeQuery){
-          fullUrl += "&" + themeQuery;
-        }
-      }else if(themeQuery){
-        fullUrl += themeQuery;
+  // construire l'url avec les filtres sélectionnés
+  if (regionQuery || themeQuery) {
+    fullUrl += "?";
+    if (regionQuery) {
+      fullUrl += regionQuery;
+      if (themeQuery) {
+        fullUrl += "&" + themeQuery;
       }
+    } else if (themeQuery) {
+      fullUrl += themeQuery;
     }
+  }
   const response = await fetch(fullUrl);
 
   if (!response.ok) {
-    console.error("Erreur lors de la récupération des données :", response.status);
+    console.error(
+      "Erreur lors de la récupération des données :",
+      response.status
+    );
     listMuseum.innerHTML = "<p>Erreur lors de la récupération des données.</p>";
     return; // Arrête l'exécution de la fonction en cas d'erreur
   }
 
   const data = await response.json();
-  const searchResults = data.results;  // variable qui stocke les résultats de la recherche
-
+  const searchResults = data.results; // variable qui stocke les résultats de la recherche
 
   // Affiche le nombre de résultats
   const resultCount = searchResults.length;
   const resultCountElement = document.createElement("p");
-  resultCountElement.textContent = `Nombre de résultats : ${resultCount}`;
+  resultCountElement.innerText = `Nombre de résultats : ${resultCount}`;
   resultCountElement.classList.add("compteurReponses");
-
-  // Ajoute le compteur avant la liste des musées
-  listMuseum.prepend(resultCountElement);
 
   if (resultCount === 0) {
     listMuseum.innerHTML += "<p>Aucun résultat pour votre recherche.</p>";
   } else {
     showMuseum(searchResults);
   }
-
-  // Appel à une fonction dans script.js pour afficher les résultats
-  // showMuseum(searchResults);
+  // Ajoute le compteur avant la liste des musées
+  resultsCountDiv.innerText = "";
+  resultsCountDiv.appendChild(resultCountElement);
 }
 
 selectRegion.addEventListener("change", showSearchResults);
 selectTheme.addEventListener("change", showSearchResults);
-  // selectRegion.addEventListener('change', async function() {
-  //     const url = this.options[this.selectedIndex].dataset.url;
-  //     if (selectRegion.value) {
-  //       const response = await fetch(url);
-  //       if (response.ok) {
-  //           const data = await response.json();
-  //           showSearchResults(data.results);
-  //       } else {
-  //           console.error("Erreur lors de la récupération des données :", response.status);
-  //           listMuseum.innerHTML = "<p>Erreur lors de la récupération des données.</p>";
-  //       }
-  //     }
-  //   });
+
+// selectRegion.addEventListener('change', async function() {
+//     const url = this.options[this.selectedIndex].dataset.url;
+//     if (selectRegion.value) {
+//       const response = await fetch(url);
+//       if (response.ok) {
+//           const data = await response.json();
+//           showSearchResults(data.results);
+//       } else {
+//           console.error("Erreur lors de la récupération des données :", response.status);
+//           listMuseum.innerHTML = "<p>Erreur lors de la récupération des données.</p>";
+//       }
+//     }
+//   });
 
 //}
-
-
-
 
 // RECHERCHE PAR REGION
 // --------------------
@@ -179,7 +267,6 @@ selectTheme.addEventListener("change", showSearchResults);
 //     }
 //   }
 // });
-
 
 // function showByRegion(region){
 // 	listMuseum.innerHTML = "";
@@ -198,7 +285,7 @@ selectTheme.addEventListener("change", showSearchResults);
 //     const siteWeb = document.createElement("p");
 //     const resume = document.createElement("p");
 //     const themes = document.createElement("p");
-    
+
 //     divRegion.classList.add("museumDiv");
 //     museName.classList.add("museumName");
 //     museCity.classList.add("museumCity");
@@ -216,33 +303,32 @@ selectTheme.addEventListener("change", showSearchResults);
 //     newImageRegion.src = "./images/ticket_musee.png";
 //     moreInfoButton.innerText = "En savoir +";
 
-    // museName.innerText = region[i].nom_officiel;
-    // adresse.innerText = `${region[i].adresse},${region[i].code_postal}, ${region[i].ville}`;
-    // telephone.innerText = `Téléphone :  ${region[i].telephone}`;
-    // siteWeb.innerText = `Site web :  ${region[i].url}`;
-    // resume.innerText = `Histoire : ${region[i].histoire}`;
-    // themes.innerText = `Thématiques associées : ${region[i].themes}`;
+// museName.innerText = region[i].nom_officiel;
+// adresse.innerText = `${region[i].adresse},${region[i].code_postal}, ${region[i].ville}`;
+// telephone.innerText = `Téléphone :  ${region[i].telephone}`;
+// siteWeb.innerText = `Site web :  ${region[i].url}`;
+// resume.innerText = `Histoire : ${region[i].histoire}`;
+// themes.innerText = `Thématiques associées : ${region[i].themes}`;
 
+// Ajouter un gestionnaire d'événements pour le bouton "En savoir plus"
+//   moreInfoButton.addEventListener("click", () => {
+//     openModal(region[i]);
+//   });
 
-      // Ajouter un gestionnaire d'événements pour le bouton "En savoir plus"
-    //   moreInfoButton.addEventListener("click", () => {
-    //     openModal(region[i]);
-    //   });
+// listMuseum.appendChild(divRegion);
+// divRegion.appendChild(museName);
+// divRegion.appendChild(museCity);
+// divRegion.appendChild(newImageRegion);
+// divRegion.appendChild(moreInfoButton);
 
-    // listMuseum.appendChild(divRegion);
-    // divRegion.appendChild(museName);
-    // divRegion.appendChild(museCity);
-    // divRegion.appendChild(newImageRegion);
-    // divRegion.appendChild(moreInfoButton);
- 
-    // listMuseum.appendChild(divRegion);
-    // divRegion.appendChild(museName);
-    // divRegion.appendChild(adresse);
-    // divRegion.appendChild(telephone);
-    // divRegion.appendChild(siteWeb);
-    // divRegion.appendChild(resume);
-    // divRegion.appendChild(themes);
-  //}
+// listMuseum.appendChild(divRegion);
+// divRegion.appendChild(museName);
+// divRegion.appendChild(adresse);
+// divRegion.appendChild(telephone);
+// divRegion.appendChild(siteWeb);
+// divRegion.appendChild(resume);
+// divRegion.appendChild(themes);
+//}
 //}
 
 // Fonction pour ouvrir la MODALE REGION
@@ -283,8 +369,6 @@ selectTheme.addEventListener("change", showSearchResults);
 //   })
 // }
 
-
-
 // RECHERCHE PAR THEMATIQUE
 // ------------------------
 
@@ -302,7 +386,6 @@ selectTheme.addEventListener("change", showSearchResults);
 //   }
 // });
 
-
 // function showByTheme(theme){
 // 	listMuseum.innerHTML = "";
 
@@ -312,7 +395,7 @@ selectTheme.addEventListener("change", showSearchResults);
 //     const museCityTheme = document.createElement("h3");
 //     const newImageTheme = document.createElement("img");
 //     const moreInfoButtonTheme = document.createElement("button");
-    
+
 //     divTheme.classList.add("museumDiv");
 //     museNameTheme.classList.add("museumName");
 //     museCityTheme.classList.add("museumCity");
@@ -324,8 +407,7 @@ selectTheme.addEventListener("change", showSearchResults);
 //     newImageTheme.src = "./images/ticket_musee.png";
 //     moreInfoButtonTheme.innerText = "En savoir +";
 
-
-  // Ajouter un gestionnaire d'événements pour le bouton "En savoir plus"
+// Ajouter un gestionnaire d'événements pour le bouton "En savoir plus"
 //   moreInfoButtonTheme.addEventListener("click", () => {
 //     openModal(domaine_thematique[i]);
 //   });
@@ -334,4 +416,3 @@ selectTheme.addEventListener("change", showSearchResults);
 //     divTheme.appendChild(moreInfoButtonTheme);
 //  }
 // }
-
