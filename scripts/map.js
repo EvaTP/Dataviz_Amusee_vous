c828538d6bfb3fc8bc861ed7ce293b06e603d093
GGui26 <gui.alexandre.fernandes.ca@gmail.com>
2025-03-25 14:31:58 +0100

    filtrecarte

--- scripts/map.js
+++ scripts/map.js
@@ -1,26 +1,58 @@
+const selectTheme = document.getElementById("Themes")
+const map = L.map('mapid').setView([46.603354, 1.888334], 6);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+let markerGroup = L.layerGroup().addTo(map)
+
 async function getMuseums() {
-    const response = await fetch("https://www.data.gouv.fr/fr/datasets/r/a5c5d76e-979a-4073-ba0d-0844bb3c1398");
+    let apiUrl =
+  "https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/musees-de-france-base-museofile/records?limit=100";
+    if (selectTheme.value) {
+        apiUrl += `&${selectTheme.value}`
+    }
+    const response = await fetch(apiUrl);
     const data = await response.json();
-    console.log(data[0].coordonnees)
+    const total = data.total_count;
+    let offset = 100;
+    let museums = data.results;
+    while (offset <= total) {
+        const res = await fetch(`${apiUrl}&offset=${offset}`)
+        const dat = await res.json();
+        dat.results.forEach(result => {
+            museums.push(result)
+        })
+        offset += 100;
+    }
+    console.log(museums)
 
 
     
-    function putmarker(data) {
     
-        const map = L.map('mapid').setView([46.603354, 1.888334], 6);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+
     
-        data.forEach(item => {
+        museums.forEach(item => {
             const longitude = item.coordonnees.lon
             const latitude = item.coordonnees.lat
-            console.log(latitude,longitude)
-            L.marker([latitude, longitude]).addTo(map)
-                .bindPopup("Nom: "+item.nom_officiel+"<br>"+"Adresse: "+item.adresse+"<br>"+"Code postal: "+item.code_postal+"<br>"+"Ville: "+item.ville+"<br>"+"Région: "+item.region+"<br>"+" Département: "+item.departement+"<br>"+"URL: "+item.url)
+            L.marker([latitude, longitude]).addTo(markerGroup)
+                .bindPopup("Nom: "+(item.nom_officiel? item.nom_officiel : "Non disponible" )+ "<br>"+
+                    "Adresse: "+(item.adresse? item.adresse : "Non disponible" )+ "<br>"+
+                    "Code postal: "+(item.code_postal ? item.code_postal : "Non disponible")+"<br>"+
+                    "Ville: "+(item.ville ? item.region : "Non disponible") +"<br>" +
+                    "Région: "+(item.region ? item.region : "Non disponible")+"<br>"+
+                    "Département: "+(item.departement ? item.departement : "Non disponible")+"<br>"+
+                    "Site Web: " + (item.url ? '<a href="http://' + item.url + '" target="_blank">' + item.url + '</a>' : "Non disponible"))
+                    
+
+                    
+                    
+
         })
-    }
-    putmarker(data)
 }
 getMuseums()
 
+selectTheme.addEventListener("change", () => {
+    markerGroup.clearLayers();
+    getMuseums();
+})
